ec7b228853e02096832677c86d91fd9f159e2cbc
Master <baek011226@naver.com>
2025-03-04 20:19:39 +0900

    lv1, lv2 수정

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -18,31 +18,41 @@ public class Calculator {
         // 결과 저장 변수
         int calculationResult;
 
-        if (operator.equals("+")) {
-            calculationResult = num1 + num2;
-        } else if (operator.equals("-")) {
-            calculationResult = num1 - num2;
-        } else if (operator.equals("*")) {
-            calculationResult = num1 * num2;
-        } else if (operator.equals("/")) {
-            if (num2 == 0) {
-                // 예외 처리
-                throw new RuntimeException("0으로 나눌 수 없습니다.");
-            } else {
+        // if 문 보다 더 간결함
+        switch (operator) {
+            case "+":
+                calculationResult = num1 + num2;
+                break;
+            case "-":
+                calculationResult = num1 - num2;
+                break;
+            case "*":
+                calculationResult = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                }
                 calculationResult = num1 / num2;
-            }
-        } else {
-            throw new RuntimeException("잘못 입력하셨습니다. 다시 입력해주세요");
+                break;
+            default:
+                throw new IllegalArgumentException("잘못된 연산자입니다. (+, -, *, / 중 하나를 입력해주세요)");
         }
 
         // result 리스트에 추가
-        this.result.add(calculationResult);
+        result.add(calculationResult);
 
-        return this.result;
+        // 데이터 보호 강화: 외부에서 리스트 수정 불가
+        return Collections.unmodifiableList(result);
     }
 
     // 삭제 메소드
     public void removeResult() {
+        // result가 비어있을때 대비
+        if (result.isEmpty()) {
+            System.out.println("저장된 결과가 없습니다.");
+            return;
+        }
         result.remove(0);
         System.out.println("첫번째 결과가 제거 되었습니다.");
         System.out.println(result);
@@ -50,7 +60,8 @@ public class Calculator {
 
     // getter
     public List<Integer> getResult() {
-        return result;
+        // 데이터 보호 강화: 리스트를 외부에서 수정할 수 없음
+        return Collections.unmodifiableList(result);
     }
 
     // setter
--- src/lv2/Main.java
+++ src/lv2/Main.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = 0;
@@ -13,51 +14,22 @@ public class Main {
         Calculator calc = new Calculator();
         while (true) {
             try {
-                while (true) {
-                    System.out.println("첫번째 숫자를 입력하세요: ");
-                    num1 = sc.nextInt();
-                    if (num1 < 0) {
-                        System.out.println("양의 정수를 입력해주세요");
-                    } else {
-                        break;
-                    }
-                }
+                num1 = getNumber(sc, "첫번째 숫자를 입력해주세요.");
+                num2 = getNumber(sc, "두번째 숫자를 입력해주세요.");
+                operator = getOperator(sc);
 
-                while (true) {
-                    System.out.println("두번째 숫자를 입력하세요: ");
-                    num2 = sc.nextInt();
-                    if (num2 < 0) {
-                        System.out.println("양의 정수를 입력해주세요");
-                    } else {
-                        break;
-                    }
-                }
-
-                while (true) {
-                    System.out.println("사칙연산 기호를 입력하세요: ");
-                    operator = sc.next();
-                    // 정규 표현식 사용
-                    if (operator.length() == 1 && operator.matches("[+\\-*/]")) {
-                        break;
-                    } else {
-                        System.out.println("사칙 연산 기호를 다시 입력해주세요");
-                    }
-                }
-
-                calc.setResult(calc.calculate(num1, num2, operator));
+                calc.calculate(num1, num2, operator);
                 System.out.println("결과:" + calc.getResult());
 
             } catch (InputMismatchException e) {
                 System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
                 sc.nextLine();
                 continue;
-            } catch (RuntimeException e) {
+            } catch (ArithmeticException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
                 continue;
             }
-
-            sc.nextLine();
 
             System.out.println("첫 번째 데이터 삭제하기 - (yes/no)");
             String removeResult = sc.nextLine();
@@ -74,5 +46,35 @@ public class Main {
             }
         }
         sc.close();
+    }
+
+    // 숫자 입력 메서드
+    private static int getNumber(Scanner sc, String message) {
+        while (true) {
+            System.out.println(message);
+            try {
+                int num = Integer.parseInt(sc.nextLine());
+                if (num < 0) {
+                    System.out.println("양의 정수를 입력해주세요");
+                } else {
+                    return num;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("숫자만 입력 가능합니다. 다시 입력해주세요");
+            }
+        }
+    }
+
+    // 사칙연산 기호 메서드
+    private static String getOperator(Scanner sc){
+        while(true){
+            System.out.println("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+            String operator = sc.nextLine();
+            if(operator.matches("[+\\-*/]")){
+                return operator;
+            }else{
+                System.out.println("올바른 사칙연산 기호를 입력해주세요.");
+            }
+        }
     }
 }
